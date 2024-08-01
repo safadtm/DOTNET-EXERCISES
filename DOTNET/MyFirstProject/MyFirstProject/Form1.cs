e2a50a6ada013b544d043572a1f03ddffb1de8ad
safadtm <safadtm2004@gmail.com>
2024-08-01 09:06:15 +0530

    First App

---
+++ DOTNET/MyFirstProject/MyFirstProject/Form1.cs
@@ -0,0 +1,16 @@
+namespace MyFirstProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = username.Text;
+            MessageBox.Show("Welcome "+name);
+        }
+    }
+}
