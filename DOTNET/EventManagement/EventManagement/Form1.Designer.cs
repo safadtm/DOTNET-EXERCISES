2bc60a1439dc1a774080173dae9b19863eaad91b
safadtm <safadtm2004@gmail.com>
2024-08-02 20:10:29 +0530

    EventManagement -starter

---
+++ DOTNET/EventManagement/EventManagement/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace EventManagement
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            label4 = new Label();
+            label5 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(72, 68);
+            label1.Name = "label1";
+            label1.Size = new Size(64, 28);
+            label1.TabIndex = 0;
+            label1.Text = "Name";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(75, 125);
+            label2.Name = "label2";
+            label2.Size = new Size(57, 28);
+            label2.TabIndex = 1;
+            label2.Text = "Place";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(75, 177);
+            label3.Name = "label3";
+            label3.Size = new Size(67, 28);
+            label3.TabIndex = 2;
+            label3.Text = "Phone";
+            label3.Click += label3_Click;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(151, 258);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(76, 32);
+            radioButton1.TabIndex = 3;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Male";
+            radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.CheckedChanged += radioButton1_CheckedChanged;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(254, 256);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(95, 32);
+            radioButton2.TabIndex = 4;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Female";
+            radioButton2.UseVisualStyleBackColor = true;
+            radioButton2.CheckedChanged += radioButton2_CheckedChanged;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(72, 333);
+            label4.Name = "label4";
+            label4.Size = new Size(59, 28);
+            label4.TabIndex = 5;
+            label4.Text = "Email";
+            label4.Click += label4_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(72, 395);
+            label5.Name = "label5";
+            label5.Size = new Size(93, 28);
+            label5.TabIndex = 6;
+            label5.Text = "Password";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(185, 68);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(197, 34);
+            textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(185, 125);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(197, 34);
+            textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(185, 185);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(197, 34);
+            textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(185, 336);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(197, 34);
+            textBox4.TabIndex = 10;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(185, 395);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(197, 34);
+            textBox5.TabIndex = 11;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(83, 492);
+            button1.Name = "button1";
+            button1.Size = new Size(127, 64);
+            button1.TabIndex = 12;
+            button1.Text = "Clear";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(276, 492);
+            button2.Name = "button2";
+            button2.Size = new Size(125, 64);
+            button2.TabIndex = 13;
+            button2.Text = "Register";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(457, 492);
+            button3.Name = "button3";
+            button3.Size = new Size(125, 64);
+            button3.TabIndex = 14;
+            button3.Text = "Login";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(11F, 28F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(929, 585);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Event Management";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private Label label4;
+        private Label label5;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+    }
+}
---
+++ DOTNET/EventManagement/EventManagement/Form1.cs
@@ -0,0 +1,53 @@
+namespace EventManagement
+{
+    
+    public partial class Form1 : Form
+    {
+        
+        string gender;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            gender = "Male";
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            gender = "Female";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
